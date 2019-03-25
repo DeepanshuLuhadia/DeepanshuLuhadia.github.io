8bbedb03e408ac6f869b0a9141a92b501b535f86
Deepanshu Jain <27558235+DeepanshuLuhadia@users.noreply.github.com>
2019-03-25 18:04:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Chandan Steels</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+		<nav class="navbar navbar-expand-sm bg-light navbar-light ">
+			  <!-- Brand -->
+			
+
+			  <!-- Links -->
+				<ul class="navbar-nav" style="margin-left: auto !important">
+			    	<li class="nav-item">
+			    		  <a class="nav-link text-dark" href="#"><i class="fa fa-map-marker text-danger" aria-hidden="true"></i>Address</a>
+			    	</li>
+			    	<li class="nav-item">
+			    		  <a class="nav-link text-dark" href="#"><i class="fa fa-phone text-dark" aria-hidden="true"></i>7610090041</a>
+			    	</li>
+			    	<li class="nav-item">
+			    		  <a class="nav-link text-dark" href="#"><i class="fa fa-envelope text-warning" aria-hidden="true"></i>Deepanshujain388@gmail.com</a>
+			    	</li>
+
+			    
+			  	</ul>
+		</nav>
+
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+			<div class="navbar-brand " href="#" style="position: relative;">
+				<div class=" logo" style="position:absolute; width: 200px; height: 150px;">
+					<img src="logow.png" alt="logo" style="width:100%; height: 100%;">
+				</div>
+			</div>
+			
+			<ul class="navbar-nav" style="margin-left: 100px;">
+			<!-- Dropdown -->
+
+		      	<li class="nav-item">
+		        	<a class="nav-link" href="#">Home</a>
+		      	</li>
+		      	<li class="nav-item">
+		        	<a class="nav-link" href="#">About</a>
+		    	</li>
+		    	<li class="nav-item dropdown">
+		    		<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+		        		Services
+				    </a>
+					    <div class="dropdown-menu">
+					        <a class="dropdown-item" href="#">Link 1</a>
+				    	    <a class="dropdown-item" href="#">Link 2</a>
+				        	<a class="dropdown-item" href="#">Link 3</a>
+				      	</div>
+		    	</li>
+		     	<li class="nav-item">
+		        	<a class="nav-link" href="#">Blog</a>
+		      	</li>
+		      	<li class="nav-item">
+		        	<a class="nav-link" href="#">Contact</a>
+		      	<li class="nav-item" style="margin-left: auto;">
+		        	
+		        	<a class="nav-link" href="#"> <input type="search" name="search" placeholder="search" class="bg-dark text-light form-control" > </a>
+		      	</li>
+		      	</li>
+		      	<li class="nav-item">
+		      		<a href=""><i class="fa fa-search" aria-hidden="true"></i></a>
+		      	</li>
+		     
+
+			</ul>
+		
+		</nav>
+	
+	<div class="container">
+	<div class="slider">
+		<div class="row content-heading">
+		</div>
+			<div class=" col-lg-12">
+				<h1 style="">We Build Your Dream</h1>
+				<p>Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.	</p>
+				<button class="secondary-button"> Learn More</button>
+			</div>
+	</div>
+		<div class="section-mid">
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="about">
+						<div class="images">
+							<img src="1.jpeg" alt="image1" style="width: 100%; height: 100%;">
+						</div>
+						<h3>Certified Experience</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="#">
+							<span>Read More</span>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="about">
+					<div class="images">
+						<img src="2.jpg" alt="image1" style="width: 100%; height: 100%;"></div>
+						<h3>Certified Experience</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="#">
+							<span>Read More</span>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="about">
+					<div class="images">
+						<img src="3.jpeg" alt="image1" style="width: 100%; height: 100%;"></div>
+						<h3>Certified Experience</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="#">
+							<span>Read More</span>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="section md-section" >
+					<div class= "bg-img bg-half overlay" style="background-image: url(4.jpg); position: absolute;"></div>
+			<div class="container">
+				<div class="row">
+				
+				<div class="data col-lg-offset-3 col-lg-5"><h4>Subtitle</h4>
+				<h2 class="title"> About Us</h2>
+				 <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="row number">
+			
+				<div class="number-data col-md-12">
+					<div class="col-md-3">
+					<div class="font col-md-offset-3 col-md-2"><h1><i class="fa fa-building" aria-hidden="true"></i></h1></div>
+					<div class="col-lg-7"><h3>20</h3><span>year of experience</span></div>
+					</div>
+					<div class="col-md-3">
+					<div class="font col-md-offset-3 col-md-2"><h1><i class="fa fa-university" aria-hidden="true"></i></h1></div>
+					<div class="col-lg-7"><h3>20</h3><span>year of experience</span></div>
+					</div>
+					<div class="col-md-3">
+					<div class="font col-md-offset-3 col-md-2"><h1><i class="fa fa-handshake-o" aria-hidden="true"></i></h1></div>
+					<div class="col-md-7"><h3>20</h3><span>year of experience</span></div>
+					</div>
+					<div class="col-md-3">
+					<div class="font col-md-offset-3 col-md-2"><h1><i class="fa fa-trophy" aria-hidden="true"></i></h1></div>
+					<div class="col-md-7"><h3>20</h3><span>year of experience</span></div>
+					</div>
+					
+				</div>
+			
+		</div>
+	</div>
+</body>
+</html>
